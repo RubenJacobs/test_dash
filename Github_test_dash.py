1e7bf03f16054dab59abaf980c5101e3e2e1760b
RubenJacobs <ruben.jacobs@leyden-jar.com>
2024-08-10 13:46:50 +0200

    Add files via upload

---
+++ Github_test_dash.py
@@ -0,0 +1,16 @@
+from dash import Dash, dcc, html, Input, Output,callback
+app = Dash(__name__)
+app.layout = html.Div([
+    dcc.Dropdown(['blue','green','yellow'], id='color_dropdown'),
+    html.Div(id='color_output')
+])
+
+@callback(
+    Output('color_output', 'children'),
+    Input('color_dropdown', 'value')
+)
+def update_output(value):
+    return f'You have selected {value}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
